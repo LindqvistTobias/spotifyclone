da2bf3415af9f3cefbed063781d6b4ede6a4fa51
Tobias <lokwavetechnologies@gmail.com>
2024-05-11 13:10:29 +0200

    Added playerfunctions

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,5 +1,6 @@
 import { Box, Grid, Typography, Avatar } from '@mui/material';
 import { useEffect, useState } from 'react';
+import PlayerControls from '../PlayerControls/PlayerControls';
 
 const Player = ({ spotifyApi, token }) => {
 	const [localPlayer, setLocalPlayer] = useState();
@@ -8,6 +9,7 @@ const Player = ({ spotifyApi, token }) => {
 	const [device, setDevice] = useState();
 	const [duration, setDuration] = useState();
 	const [progress, setProgress] = useState();
+	const [active, setActive] = useState();
 
 	useEffect(() => {
 		const script = document.createElement('script');
@@ -35,23 +37,50 @@ const Player = ({ spotifyApi, token }) => {
 				console.log('Device ID has gone offline', device_id);
 			});
 
-			player.addListener('player_state_changed', (state) => {				
-                if(!state || !state.track_window?.current_track) {
-                    return;
-                }
-                console.log(state);
+			player.addListener('player_state_changed', (state) => {
+				if (!state || !state.track_window?.current_track) {
+					return;
+				}
+				console.log(state);
 
-                const duration = state.track_window.current_track.duration_ms / 1000;
-                const progress = state.position / 1000;
-                setDuration(duration);
-                setProgress(progress);
-                setIsPaused(state.paused)
-                setCurrentTrack(state.track_window.current_track)
+				const duration = state.track_window.current_track.duration_ms / 1000;
+				const progress = state.position / 1000;
+				setDuration(duration);
+				setProgress(progress);
+				setIsPaused(state.paused);
+				setCurrentTrack(state.track_window.current_track);
+
+				player.getCurrentState().then((state) => {
+					!state ? setActive(false) : setActive(true);
+				});
 			});
 
 			player.connect();
 		};
 	}, []);
+
+	useEffect(() => {
+		if (!localPlayer) return;
+		async function connect() {
+			await localPlayer.connect();
+		}
+
+		connect();
+		return () => {
+			localPlayer.disconnect();
+		};
+	}, [localPlayer]);
+
+	// useEffect(() => {
+	// 	const transferPlayback = async () => {
+	// 		if (device) {
+	// 			const res = await spotifyApi.getMyDevices();
+	// 			console.log(res);
+	// 			await spotifyApi.transferMyPlayback([device], false);
+	// 		}
+	// 	};
+	// 	transferPlayback();
+	// }, [device, spotifyApi]);
 
 	return (
 		<Box>
@@ -66,11 +95,18 @@ const Player = ({ spotifyApi, token }) => {
 					borderTop: '1px solid #292929'
 				}}
 			>
-				<Grid xs={12} md={4} items sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
-					<Avatar src={null} alt={null} variant="square" sx={{ width: 56, height: 56, marginRight: 2 }} />
+				<Grid xs={12} md={4} item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+					<Avatar
+						src={current_track?.album.images[0].url}
+						alt={current_track?.album.name}
+						variant="square"
+						sx={{ width: 56, height: 56, marginRight: 2 }}
+					/>
 					<Box>
-						<Typography sx={{ color: 'text.primary', fontSize: 14 }}>Cant take a joke</Typography>
-						<Typography sx={{ color: 'text.secondary', fontSize: 10 }}>Drake</Typography>
+						<Typography sx={{ color: 'text.primary', fontSize: 14 }}>{current_track?.name}</Typography>
+						<Typography sx={{ color: 'text.secondary', fontSize: 10 }}>
+							{current_track?.artists[0].name}
+						</Typography>
 					</Box>
 				</Grid>
 				<Grid
@@ -80,11 +116,20 @@ const Player = ({ spotifyApi, token }) => {
 						alignItems: 'center'
 					}}
 					md={4}
-					items
+					item
 				>
-					Play knappen
+					{active ? (
+						<PlayerControls
+							progress={progress}
+							is_paused={is_paused}
+							duration={duration}
+							player={localPlayer}
+						/>
+					) : (
+						<Box>Please transfer Playback</Box>
+					)}
 				</Grid>
-				<Grid xs={6} md={4} items sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+				<Grid xs={6} md={4} item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
 					Volume
 				</Grid>
 			</Grid>
